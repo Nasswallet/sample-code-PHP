7b716f6fdbba293b443f4516ca3fe9ba66802b46
Nasswallet <omar.ali@nasswallet.com>
2020-10-25 14:55:53 +0300

    initail commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+header('Content-type: text/html; charset=utf-8');
+
+
+use GuzzleHttp\Client;
+use Guzzle\Http\EntityBody;
+use Guzzle\Http\Message\Request;
+use Guzzle\Http\Message\Response;
+use GuzzleHttp\Exception\RequestException;
+
+
+require 'vendor/autoload.php';
+
+require 'Payment.php';
+
+
+
+
+if (isset($_POST['action']) == 'pay') {
+    $payment = new Payment();
+    return $payment->makePayment();
+}
+
+
+ 
+
+
+    
+ 
+require 'index.view.php';
